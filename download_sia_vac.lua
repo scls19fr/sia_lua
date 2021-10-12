fb75bd8d772a2e9c46cc960d88982102a642255e
scls19fr <s.celles@gmail.com>
2021-10-12 21:00:14 +0200

    wip

---
+++ download_sia_vac.lua
@@ -0,0 +1,19 @@
+local BASE_URL_SIA = "https://www.sia.aviation-civile.gouv.fr"
+
+local http = require("socket.http")
+
+function download_sia_vac(code)
+    -- local airac_string = format_french_date(dt_airac)
+    local airac_string = "07_OCT_2021"
+    local endpoint = string.format("/dvd/eAIP_%s/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.%s.pdf", airac_string, code);
+    local url = BASE_URL_SIA + endpoint;
+    local body, code = http.request(url);
+    if not body then error(code) end
+    local fname = string.format("AD-2.%s.pdf", code);
+    local f = assert(io.open(fname, 'wb')) -- open in "binary" mode
+    f:write(body)
+    return f:close();
+end
+
+print(BASE_URL_SIA)
+print(download_sia_vac("LFBI"))
