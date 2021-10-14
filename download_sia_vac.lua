a490a41ab06c8767f3c9b4df9fb6de90f5e7f06b
Sébastien Celles <scls19fr@users.noreply.github.com>
2021-10-14 10:14:10 +0200

    Update download_sia_vac.lua

--- download_sia_vac.lua
+++ download_sia_vac.lua
@@ -6,7 +6,7 @@ function download_sia_vac(code)
     -- local airac_string = format_french_date(dt_airac)
     local airac_string = "07_OCT_2021"
     local endpoint = string.format("/dvd/eAIP_%s/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.%s.pdf", airac_string, code);
-    local url = BASE_URL_SIA + endpoint;
+    local url = BASE_URL_SIA .. endpoint;
     local body, code = http.request(url);
     if not body then error(code) end
     local fname = string.format("AD-2.%s.pdf", code);
