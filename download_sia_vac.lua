df182b903a1f1d315f15f26a8cf89561e2d657b0
scls19fr <s.celles@gmail.com>
2021-10-17 09:35:54 +0200

    use https request

--- download_sia_vac.lua
+++ download_sia_vac.lua
@@ -1,16 +1,16 @@
 local BASE_URL_SIA = "https://www.sia.aviation-civile.gouv.fr"
 
-local http = require("socket.http")
+local https = require("ssl.https")
 
-function download_sia_vac(code)
+local function download_sia_vac(oaci_code)
     -- local airac_string = format_french_date(dt_airac)
     local airac_string = "07_OCT_2021"
-    local endpoint = string.format("/dvd/eAIP_%s/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.%s.pdf", airac_string, code)
+    local endpoint = string.format("/dvd/eAIP_%s/Atlas-VAC/PDF_AIPparSSection/VAC/AD/AD-2.%s.pdf", airac_string, oaci_code)
     local url = BASE_URL_SIA .. endpoint
     print(string.format("Download VAC from %s", url))
-    local body, ret_code = http.request(url)
-    if not body then error(ret_code) end
-    local fname = string.format("AD-2.%s.pdf", code)
+    local body, code, headers, status = https.request(url)
+    if not body then error(code) end
+    local fname = string.format("AD-2.%s.pdf", oaci_code)
     local f = assert(io.open(fname, 'wb')) -- open in "binary" mode
     f:write(body)
     return f:close()
